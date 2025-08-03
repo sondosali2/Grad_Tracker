56d37320cb6af91fab45f8f0fa33d1f4f0d9f23f
sondosali2 <sondosali840@gmail.com>
2025-08-03 14:44:48 +0300

    Added DevConnecters

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    await mongoose
-        .connect(process.env.MONGO_URI)
-        .then(() => {
-            console.log("MongoDB Connected");   
-        })
-        .catch((err) => {
-            console.error(err);
-            process.exit(1);
-            });
-            };
+    const mongoUri = process.env.MONGO_URI;
+
+    if (!mongoUri) {
+        console.error("MONGO_URI is missing. Make sure it is set in Railway environment variables.");
+        process.exit(1);
+    }
+
+    try {
+        await mongoose.connect(mongoUri);
+        console.log("MongoDB Connected");
+    } catch (err) {
+        console.error("MongoDB Connection Failed:", err);
+        process.exit(1);
+    }
+};
 
 export default connectDB;
